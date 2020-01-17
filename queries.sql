7816a445a0546e982a55ee4e202d523b54c40f4d
jimmynetor3 <54798823+jimmynetor3@users.noreply.github.com>
2020-01-17 11:46:13 +0100

    Uitwerking van de exercise

---
+++ queries.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `planeten` ADD `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY
+INSERT INTO sterrenstelsel.planeten (naam, diameter, `Afstand tot de zon`, `massa t.o.v. de aarde`, bezoek_datum, id) VALUES ('Mars', 6794, 227936640, 0, null, null)
